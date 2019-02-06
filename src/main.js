457dc9508b91f19718fafe054bdd9cbf8982b3dd
thehao <chungthehao@gmail.com>
2019-02-06 11:46:41 +0700

    Auth Module State and Getters

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App';
+import store from './store'; // ko cần './store/index' webpack tự kiếm index.js
 
 new Vue({
+    store,
     render: h => h(App)
 }).$mount('#app');
---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+// Allow Vuex to somehow talk to Vue in general
+// connect 2 library với nhau
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: {}
+});
